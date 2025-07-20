c2cc86906d998c57aaba51339ca7a8f4d2ff2281
Michael Frieze <michaelfrieze@icloud.com>
2025-07-19 23:36:42 -0400

    fix: set customViteReactPlugin to true and add react to vite config

--- vite.config.ts
+++ vite.config.ts
@@ -2,15 +2,18 @@ import { defineConfig } from 'vite'
 import { tanstackStart } from '@tanstack/react-start/plugin/vite'
 import viteTsConfigPaths from 'vite-tsconfig-paths'
 import tailwindcss from '@tailwindcss/vite'
+import react from '@vitejs/plugin-react'
 
 const config = defineConfig({
   plugins: [
-    // this is the plugin that enables path aliases
     viteTsConfigPaths({
       projects: ['./tsconfig.json'],
     }),
     tailwindcss(),
-    tanstackStart(),
+    tanstackStart({
+      customViteReactPlugin: true,
+    }),
+    react(),
   ],
 })
 
