908e9a0ad635fb68ec2eb9d270e27089c90677d3
Michael Frieze <michaelfrieze@icloud.com>
2025-07-21 18:46:49 -0400

    feat: setup default error and not found components

--- src/router.tsx
+++ src/router.tsx
@@ -1,11 +1,12 @@
 import { createRouter as createTanstackRouter } from '@tanstack/react-router'
 import { routerWithQueryClient } from '@tanstack/react-router-with-query'
+import { DefaultCatchBoundary } from './components/default-catch-boundary'
+import { NotFound } from './components/not-found'
 import * as TanstackQuery from './integrations/tanstack-query/root-provider'
+import './styles.css'
 
 // Import the generated route tree
 import { routeTree } from './routeTree.gen'
-
-import './styles.css'
 
 // Create a new router instance
 export const createRouter = () => {
@@ -17,6 +18,9 @@ export const createRouter = () => {
       },
       scrollRestoration: true,
       defaultPreloadStaleTime: 0,
+      defaultPreload: 'intent',
+      defaultErrorComponent: DefaultCatchBoundary,
+      defaultNotFoundComponent: () => <NotFound />,
     }),
     TanstackQuery.getContext().queryClient,
   )
